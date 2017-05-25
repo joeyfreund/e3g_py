83bacea5e61cf9fe392107a61acc3591daf2cffb
kouritron <kourosh.lk.kh@gmail.com>
2017-05-25 10:21:10 -0400

    checkout command

--- e3g.py
+++ e3g.py
@@ -6,92 +6,67 @@ import sys
 import argparse
 import getpass
 
-from libe3g.error import *
+
 from libe3g import cryptmanager as cm
 from libe3g import cryptmode
 from libe3g import log
 from libe3g import util
 
 
+def _process_checkout_subcommand(dest):
+    """ Process the checkout subcommand. Will block and ask user for the passphrase for this secret folder. """
 
-def _init_new_secret_folder222(folder_name, usr_pass):
-    """ Initialize the on disk structure for a new secret directory with the given name. """
+    log.fefr("_process_checkout_subcommand() called with dest: " + str(dest))
 
-    assert None != folder_name
-    assert (isinstance(folder_name, str) or isinstance(folder_name, unicode))
+    # TODO we probably dont have to delete everything in secret folder and extract them from the shadow.
+    # this is a simple early implementation tho.
 
-    # example folder_name: "./sf"  shadow >>> "./sf_shadow"
+    sf_name = dest
+    shadow_dir = util.get_shadow_name(name=sf_name)
+    log.vvv("destination folder is: " + str(sf_name) + " shadow folder: " + str(shadow_dir))
 
-    # if "./sf" is a file - error cant init.
-    if os.path.isfile(folder_name):
-        raise E3GError("Supplied directory name exists and is a file.")
-
-    shadow_dir = util.get_shadow_name(name=folder_name)
-
-    # if "./sf_shadow" is a file - error cant init.
-    if os.path.isfile(shadow_dir):
-        raise E3GError("Can't accept the supplied directory name, because its shadow is a file.")
-
-    # if "./sf_shadow" is a non-empty directory - error cant init.
-    if os.path.isdir(shadow_dir) and (0 != len(os.listdir(shadow_dir))):
-        raise E3GError("Can't accept the supplied directory name, because its shadow is a non-empty folder.")
-
-
-    # make the secret folder and its shadow.
-    if not os.path.isdir(folder_name):
-        os.makedirs(folder_name)
-
-    if not os.path.isdir(shadow_dir):
-        os.makedirs(shadow_dir)
+    try:
+        shadow_files = os.listdir(sf_name)
+        for shadow_file in shadow_files:
+            os.remove(os.path.join(shadow_dir, shadow_file))
+    except:
+        print ("Error: Can not clear secret directory, do i have sufficient permissions? ")
+        sys.exit(1)
 
     # .e3g_public file resides in './sf' and also in './sf_shadow' in plain text. public items go in it. (i.e kdf salt)
-    dot_e3g_public = {}
-    dot_e3g_public['salt'] = cm.get_new_random_salt()
+    #  read the .e3g_public file from shadow folder and get a py dict from the JSON in it.
+    dot_e3g_public = util.get_dict_from_json_at_pathname(src_pathname=os.path.join(shadow_dir, '.e3g_public'))
+
+    # be careful with the dot_e3g_public, its content may now be unicode not str or bytes.
+    if not dot_e3g_public.has_key('salt'):
+        print "Error: .e3g_public file in " + str(shadow_dir) + " file is inaccessible or corrupted. "
+
+    # this maybe a unicode not str.
+    dot_e3g_public['salt'] = str(dot_e3g_public['salt'])
+    log.vv(".e3g_public read and decoded: " + str(dot_e3g_public))
+
+    # now ask the user for the passphrase
+    user_pass = getpass.getpass(prompt="Passphrase: ")
 
     # now that we have the salt and the user pass make a transcryptor obj that can encrypt and decrypt things.
-    tc = cm.Transcryptor(usr_pass=usr_pass, salt=dot_e3g_public['salt'])
+    tc = cm.Transcryptor(usr_pass=user_pass, salt=dot_e3g_public['salt'])
 
-    secret_folder_current_files = os.listdir(folder_name)
+    tc.decrypt_file(src=os.path.join(shadow_dir, '.e3g_protected') , dst=os.path.join(sf_name, '.e3g_protected'))
 
-    plaintext_filenames = []
-    for sf_filename in secret_folder_current_files:
-        if ('.e3g_public' != sf_filename) and ('.e3g_protected' != sf_filename):
-            plaintext_filenames.append(sf_filename)
-        else:
-            log.vv("found e3g file: " + sf_filename)
+    # now that the .e3g_protected is sitting in secret folder in plain text we can read and decode the JSON.
+    dot_e3g_protected = util.get_dict_from_json_at_pathname(src_pathname=os.path.join(sf_name, '.e3g_protected'))
 
-
-    log.vv("file names of files found at src(excluding .e3g files): " + str(plaintext_filenames))
-
-    # now we have files whose name is in plaintext_filenames list + the two .e3g files.
-
-    # .dot_e3g_protected file resides in './sf' and has its cipher text version in './sf_shadow'
-    dot_e3g_protected = {}
-    filename_mappings = [(pfname, cryptmode.get_new_random_filename()) for pfname in plaintext_filenames]
-    dot_e3g_protected['filename_mappings'] = filename_mappings
-    #log.vvv(dot_e3g['filename_mappings'])
-
-
-    # now save the .e3g_public file into both './sf' and './sf_shadow' folder
-    util.save_dict_as_json_to_pathname(dst_pathname=os.path.join(folder_name, '.e3g_public'), py_dict=dot_e3g_public)
-    util.save_dict_as_json_to_pathname(dst_pathname=os.path.join(shadow_dir, '.e3g_public'), py_dict=dot_e3g_public)
-
-    # now save the .e3g_protected file into './sf' and its ciphertext version into './sf_shadow' folder
-    util.save_dict_as_json_to_pathname(dst_pathname=os.path.join(folder_name, '.e3g_protected'), py_dict=dot_e3g_protected)
-    tc.encrypt_file(src=os.path.join(folder_name, '.e3g_protected'), dst=os.path.join(shadow_dir, '.e3g_protected'))
-
-    # now make shadow files in './sf_shadow' for everything that lives in '.sf'
-    for src_name, annon_name in dot_e3g_protected['filename_mappings']:
-        src_pathname = os.path.join(folder_name, src_name)
+    # now decrypt plaintext back out of shadow files in './sf_shadow' and put them in '.sf'
+    for plain_name, annon_name in dot_e3g_protected['filename_mappings']:
+        plain_pathname = os.path.join(sf_name, plain_name)
         annon_pathname = os.path.join(shadow_dir, annon_name)
-        log.v(src_pathname + ' >>>> ' + annon_pathname)
+        log.vvv("Decrypting " + annon_pathname[:40] + "...." + ' >>> to: >>> ' + plain_pathname)
 
-        tc.encrypt_file(src=src_pathname, dst=annon_pathname)
-
+        tc.decrypt_file(src=annon_pathname, dst=plain_pathname)
 
 
 def _process_rdy_subcommand(dest):
-    """ Process the rdy subcommand. May block and ask user some questions. """
+    """ Process the rdy subcommand. Will block and ask user for the passphrase for this secret folder. """
 
     log.fefr("_process_rdy_subcommand() called with dest: " + str(dest))
 
@@ -119,9 +94,9 @@ def _process_rdy_subcommand(dest):
     #  read the .e3g_public file from secret folder and get a py dict from the JSON in it.
     dot_e3g_public = util.get_dict_from_json_at_pathname(src_pathname=os.path.join(sf_name, '.e3g_public'))
 
-    # becareful with the dot_e3g_public, its content may now be unicode not str or bytes.
+    # be careful with the dot_e3g_public, its content may now be unicode not str or bytes.
     if not dot_e3g_public.has_key('salt'):
-        print "Error: " + str(sf_name) + " does not appear to be e3g inited secret directory. "
+        print "Error: .e3g_public file in " + str(sf_name) + " is inaccessible or corrupted. "
 
     # this maybe a unicode not str.
     dot_e3g_public['salt'] = str(dot_e3g_public['salt'])
@@ -232,20 +207,7 @@ def _process_init_subcommand(dest):
     sf_gitignore_fh.close()
 
 
-
-
-
-
-
-def _process_checkout_subcommand(dest):
-    """ Process the checkout subcommand. May block and ask user some questions. """
-
-    log.fefr("_process_checkout_subcommand() called with dest: " + str(dest))
-
-    pass
-
-
-def _parse_cmdline(arguments):
+def _parse_cmdline():
     """ Given command line arguments to the program are in the arguments list, parse and process them. """
 
     parser = argparse.ArgumentParser(description='E3G - End to End Encrypted Git')
@@ -255,55 +217,24 @@ def _parse_cmdline(arguments):
 
     # print type(args) returns  <class 'argparse.Namespace'>
     if 'init' == args.subcmd1:
-        log.vvv(">>>> init sub command called.")
+        log.vvvv(">>>> init sub command called.")
         _process_init_subcommand(dest=args.destination)
 
     elif 'rdy' == args.subcmd1:
-        log.vvv(">>>> rdy sub command called.")
+        log.vvvv(">>>> rdy sub command called.")
         _process_rdy_subcommand(dest=args.destination)
 
     elif 'checkout' == args.subcmd1:
-        log.vvv(">>>> checkout sub command called.")
+        log.vvvv(">>>> checkout sub command called.")
+        _process_checkout_subcommand(dest=args.destination)
 
     #log.vvv("argparse args: " + str(args))
 
 
 
-def debug_run_1(arguments):
-    """ Given command line arguments to the program are in the arguments list, parse and process them. """
-
-    assert isinstance(arguments, list)
-    log.fefr("_parse_cmdline() called with arguments: " + str(arguments))
-
-
-    # TODO add support 4 these subcommands
-    # initsd (init new secret dir)
-    # ms (make shadow) (rdy) ( no option == everything) (list of files or dirs == just those)
-    # us (un-shadow)
-
-    # TODO remove this, this is just for debug.
-    try:
-        shadow_files = os.listdir('./sf_shadow')
-        for shadow_file in shadow_files:
-            os.remove(os.path.join('./sf_shadow', shadow_file))
-        #os.removedirs('./sf_shadow')
-    except:
-        log.vv("Error occurred in trying to clear the shadow folder.")
-        pass
-
-    # TODO remove the hard code, ask the user for this.
-    usr_pass = 'riscvryzenplz48163264'
-
-    #_init_new_secret_folder(folder_name='./sf', usr_pass=usr_pass)
-    #_init_new_secret_folder(folder_name='sf', usr_pass=usr_pass)
-
-
 
 
 if "__main__" == __name__:
-
-    #_parse_cmdline(sys.argv[1:])
-
-    _parse_cmdline(sys.argv[1:])
+    _parse_cmdline()
 
 
